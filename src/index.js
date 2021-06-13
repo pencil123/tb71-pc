d0bf9a862ec8dc6a514ea23ae00a535d0cc02a4c
张鲁燕 <zhangluyan@jsh.com>
2021-06-13 15:59:26 +0800

    update package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-
+import 'antd/dist/antd.css';
 import './style/common.css';
 import Router from './router/index';
 
