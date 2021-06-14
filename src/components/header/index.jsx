ca7344417c975fd45fee3506fb4704d1d542c124
张鲁燕 <zhangluyan@jsh.com>
2021-06-14 20:59:22 +0800

    edit layout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 const { Header } = Layout;
+const style = { background: "#0092ff" };
 export class Index extends Component {
   render() {
-    return <Header style={headerContent}>Header</Header>;
+    return (
+      <Header style={headerContent}>
+        <Row justify="center">
+          <Col style={style} span={6}>
+            中国大陆|用户账号|手机逛淘宝
+          </Col>
+          <Col style={style} span={10}>
+            我的淘宝|购物车|收藏夹|商品分类|免费开店|千牛卖家中心|联系客服|网站导航
+          </Col>
+        </Row>
+      </Header>
+    );
   }
 }
 
 const headerContent = {
-    height: 35,
-    lineHeight: "35px",
-    borderBottom:"5px solid #fff"
-}
+  height: 35,
+  lineHeight: "35px",
+  borderBottom: "1px solid #fff",
+};
 
 export default Index;
-
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col, Image } from "antd";
 import { TimePicker } from "antd";
 
 import LoadableComponent from "@/utils/loadableComponent";
-const MyFooter = LoadableComponent(import('@/components/footer'));
-const MyHeader = LoadableComponent(import('@/components/header'));
+const MyFooter = LoadableComponent(import("@/components/footer"));
+const MyHeader = LoadableComponent(import("@/components/header"));
 
 const { Content } = Layout;
+const imgList = [
+  "https://img.alicdn.com/imgextra/i2/O1CN01R0zbQB1rI3QA5zD7E_!!6000000005607-2-tps-209-75.png",
+  "https://img.alicdn.com/imgextra/i2/O1CN01x7kTNX1HuEGhj7rRo_!!6000000000817-2-tps-209-75.png",
+  "https://img.alicdn.com/imgextra/i2/O1CN01jOXJT41YY6bY45n4v_!!6000000003070-2-tps-209-75.png",
+  "https://img.alicdn.com/imgextra/i3/O1CN01nrmR3z217eaNzY1m5_!!6000000006938-2-tps-209-75.png",
+  "https://img.alicdn.com/imgextra/i3/O1CN01VED6ho1xql80bF4px_!!6000000006495-2-tps-209-75.png",
+];
 
 export class Home extends Component {
   render() {
@@ -15,9 +22,24 @@ export class Home extends Component {
         <Layout>
           <MyHeader />
           <Content>
-            <TimePicker use12Hours />
-            <TimePicker use12Hours format="h:mm:ss A" style={{ width: 140 }} />
-            <TimePicker use12Hours format="h:mm a" />
+            <Row justify="center">
+              <Col span={16}>
+                <Row justify="center" gutter={16}>
+                  <Col span={2} offset={1}>
+                    <Image
+                      src="https://img.alicdn.com/imgextra/i1/O1CN01spsxsM1thMXZ31bBd_!!6000000005933-2-tps-90-80.png"
+                    />
+                  </Col>
+                  {imgList.map((img) => {
+                    return (
+                      <Col span={4}>
+                        <Image src={img} />
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Col>
+            </Row>
           </Content>
           <MyFooter />
         </Layout>
