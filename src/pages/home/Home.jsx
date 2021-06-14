f57425dc762df8fa4aed0bfa34d4a92e83310e72
张鲁燕 <zhangluyan@jsh.com>
2021-06-14 08:28:15 +0800

    edit pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { Layout } from "antd";
+import { TimePicker } from 'antd';
 import MyFooter from "../../components/footer";
 
 const { Header, Footer, Content } = Layout;
 
 export class Home extends Component {
+
+
   render() {
     return (
       <>
         <Layout>
           <Header>Header</Header>
-          <Content>Content</Content>
+          <Content>
+              
+    <TimePicker use12Hours  />
+    <TimePicker use12Hours format="h:mm:ss A"  style={{ width: 140 }} />
+    <TimePicker use12Hours format="h:mm a"  />
+          </Content>
           <Footer><MyFooter /></Footer>
         </Layout>
       </>
